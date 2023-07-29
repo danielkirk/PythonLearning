9a8619e1d7d074e380a49b35b58f354cbc2accd5
Daniel Kirk <imdanielkirk@gmail.com>
2023-07-28 23:05:00 -0700

    practicing with data type fundamentals

---
+++ PythonPart1.py
@@ -0,0 +1,30 @@
+# Fundamental Data types
+# int and float
+
+int
+print(type(2 + 4))
+print(2 * 4)
+print(2 - 4)
+print(2**4)
+print(5 // 4)
+print(6 % 4)
+
+float
+print(2 / 4)
+print(type(0.0001))
+
+# Math Functions
+print(round(3.1))
+print(round(3.9))
+print(abs(-20))
+
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes -> custom types
+
+# Specialized Data Types -> Modules and Packages
